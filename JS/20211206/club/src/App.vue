42d1297769bf2e29554ade56457731aa9926a31e
Link1515 <h26281660@gmail.com>
2021-12-06 16:34:34 +0800

    上傳 20211206

---
+++ JS/20211206/club/src/App.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+#app
+  Top
+  #middle
+    #left
+      Menu
+      Update
+    #right
+      marquee 歡迎光臨校園社團介紹網，參觀後請支持社團活動並熱烈參與
+      Content
+  Footer
+</template>
+
+<script>
+// 引用自己的元件
+import Top from './components/Top.vue'
+import Menu from './components/Menu.vue'
+import Update from './components/Update.vue'
+import Content from './components/Content.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'App',
+  // 引用後註冊元件
+  components: {
+    Top,
+    Menu,
+    Update,
+    Content,
+    Footer
+  }
+}
+</script>
---
+++ JS/20211206/club/src/components/Update.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+p#update 最近更新日期: {{ year }}/{{ month }}/{{ date }}
+</template>
+
+<script>
+export default {
+  name: 'Update',
+  data () {
+    return {
+      year: new Date().getFullYear(),
+      month: new Date().getMonth().toString().padStart(2, 0),
+      date: new Date().getDate().toString().padStart(2, 0)
+    }
+  }
+}
+</script>
